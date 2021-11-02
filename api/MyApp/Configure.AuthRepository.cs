c741f87742205ae21433b2f593ccdc28f4cf2174
Demis Bellot <demis.bellot@gmail.com>
2021-11-03 01:10:24 +0800

    tweak .NET 6 GH Action

--- api/MyApp/Configure.AuthRepository.cs
+++ api/MyApp/Configure.AuthRepository.cs
@@ -13,8 +13,8 @@ namespace MyApp
     // Custom User Table with extended Metadata properties
     public class AppUser : UserAuth
     {
-        public string ProfileUrl { get; set; }
-        public string LastLoginIp { get; set; }
+        public string? ProfileUrl { get; set; }
+        public string? LastLoginIp { get; set; }
         public DateTime? LastLoginDate { get; set; }
     }
 
